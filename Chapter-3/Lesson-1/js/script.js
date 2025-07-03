0b67ef3ff63286d6a7b4e8f0c31282bfd5ae0ad5
urvashi29 <urvashigolu29@gmailcom>
2025-07-03 21:36:31 +0530

    added Ch3/L1

---
+++ Chapter-3/Lesson-1/js/script.js
@@ -0,0 +1,33 @@
+// variables a, b
+var a = 'salt';//string
+var b = "sugar";//string
+
+var num = 20;//number
+
+var x;//declaration
+x = 24;//initialization or assign a value
+
+// dislay/print
+console.log(a, b);
+console.log(x);
+console.log(num);
+
+// re-declaration
+var a = 30;
+console.log(a);
+
+//
+// gstElectronics = 20;
+// 10000 + gstElectronics
+// 20000 + gstElectronics
+
+
+var isMarried = true;//Boolean
+var IsResumeReady = false;//Boolean
+
+var val = null;//null
+console.log(val);
+
+var y;
+console.log(y);//undefined
+
